0cdeaf046cf6b85a0ec9dfea2cba73386e9deb20
islamshu <islamshu12@gmail.com>
2024-12-23 12:47:03 +0300

    edit

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -37,14 +37,14 @@ class OrderController extends Controller
     public function index_shipping()
     {
         // Fetch all orders with their details
-        $orders = Order::with('details')->where('shipping',0)->get();
+        $orders = Order::with('details')->where('shipping',1)->get();
         $title = __('Orders');
         return view('dashboard.orders.index', compact('orders', 'title'));
     }
     public function guest_orders_shipping()
     {
         // Fetch all orders with their details
-        $orders = Order::with('details')->where('client_id', null)->where('shipping',0)->get();
+        $orders = Order::with('details')->where('client_id', null)->where('shipping',1)->get();
 
         $title = __('Guest Orders');
         return view('dashboard.orders.index', compact('orders', 'title'));
@@ -52,7 +52,7 @@ class OrderController extends Controller
     public function clinet_orders_shipping()
     {
         // Fetch all orders with their details
-        $orders = Order::with('details')->where('client_id', '!=', null)->where('shipping',0)->get();
+        $orders = Order::with('details')->where('client_id', '!=', null)->where('shipping',1)->get();
 
         $title = __('Clinet Orders');
         return view('dashboard.orders.index', compact('orders', 'title'));
