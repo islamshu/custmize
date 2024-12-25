645a071eb3084be656dd45592401e514e3c0556e
islamshu <islamshu12@gmail.com>
2024-12-25 20:25:36 +0200

    edit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -307,7 +307,7 @@ class ProductController extends Controller
             }
     
             $product->save();
-            // dd($product);
+            dd($product . $request->type_product);
 
     
             // تحديث الألوان
