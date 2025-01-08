3b2892805b5c2388ce59148ca1f2bb08b57ed799
islamshu <islamshu12@gmail.com>
2025-01-08 19:42:55 +0200

    edit

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -7,10 +7,22 @@ use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-    public function sendResponse($result  , $message){
-        $response = ['success' => true , 'data' => $result, 'message' => $message,'code'=>200];
-        return response()->json($response , 200)->getData(true)->header('Access-Control-Allow-Origin', '*');
-    }
+    public function sendResponse($result, $message)
+{
+    $response = [
+        'success' => true,
+        'data' => $result,
+        'message' => $message,
+        'code' => 200
+    ];
+
+    return response()
+        ->json($response, 200)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+}
+
     public function sendErrornew( $message){
         $response = ['success' => false ,'message' => $message];
         return response()->json($response , 200);
