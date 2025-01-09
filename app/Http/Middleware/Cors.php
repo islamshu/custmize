379b2e3ef0c670375b3a5427eb9457445e8941b4
islamshu <islamshu12@gmail.com>
2025-01-09 21:03:57 +0200

    edit

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,8 +19,6 @@ class Cors
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-
         return $response;
     }
 }
