d3b0babc81f503274d2fb0858f5f891852d1284a
islamshu <islamshu12@gmail.com>
2025-02-11 22:34:42 +0200

    edit

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -259,6 +259,7 @@ class CheckoutController extends BaseController
 
                 // $savedImages = $this->saveImagesFromUrls([$frontImage, $backImage,$rightSideImage,$leftSideImage]);
                 $savedLogos = $this->saveImagesFromUrls($logos);
+                dd($this->saveImagesFromUrls_single($frontImage['url']));
               $detiels =   OrderDetail::create([
                     'order_id' => $order->id,
                     'product_name' => Product::find($orderData['product_id'])->name,
