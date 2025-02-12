fa7b8e0f3b6e855c5f1babacb1d81c50741ce9d6
islamshu <islamshu12@gmail.com>
2025-02-12 13:27:36 +0200

    edit

--- resources/views/dashboard/orders/show.blade.php
+++ resources/views/dashboard/orders/show.blade.php
@@ -132,6 +132,7 @@
                                                             <div class="d-flex flex-wrap">
                                                                 @php
                                                                     $dummy_image = asset('images/dummy.png');
+                                                                    dd( $item->productImages['front_images'])
                                                                     $images = [
                                                                         'front' => [
                                                                             'src' => $item->front_image,
