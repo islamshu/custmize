a5822976be80e5c8c2c8b69a45287852d4c8ae51
islamshu <islamshu12@gmail.com>
2025-03-13 15:12:40 +0200

    edit

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'storage/*'], // Paths to apply CORS
     'allowed_methods' => ['*'], // Allowed HTTP methods
-    'allowed_origins' => ['https://printlogo.custmize.digitalgo.net'], // Allowed origins
+    'allowed_origins' => ['https://printlogo.custmize.digitalgo.net','https://front.custmize.digitalgo.net'], // Allowed origins
     'allowed_headers' => ['*'], // Allowed headers
     'exposed_headers' => [], // Exposed headers
     'max_age' => 0, // Max age
