29ad9e69a4644bd415975739156dca2550e0508f
islamshu <islamshu12@gmail.com>
2025-06-14 11:50:19 +0300

    edit

---
+++ app/Jobs/SaveApiToFileJob.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SaveApiToFileJob implements ShouldQueue
+{
+  
+}
